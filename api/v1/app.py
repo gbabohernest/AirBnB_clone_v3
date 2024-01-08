66fe38b3d5ca9d28275190bf694e24d2d3cf0bb9
Ernest <gbaboh.ernest@gmail.com>
2024-01-08 21:26:12 +0000

    404 not found JSON custom page

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
    blueprints app_views to flask instance app
 """
 from flask import Flask
+from flask.json import jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -15,6 +16,14 @@ app.register_blueprint(app_views)
 def close_storage(obj):
     """ calls storage.close() """
     storage.close()
+
+
+@app.errorhandler(404)
+def bad_request_404(e):
+    """Handler for 404 errors.
+    return: JSON-formatted 404 status code
+    """
+    return 'error: Not found', 404
 
 
 if __name__ == "__main__":
